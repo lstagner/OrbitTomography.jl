2494d88403efba8bddb29f6d53e9f2db043a861c
Luke Stagner <stagnerl@fusion.gat.com>
2019-01-22 17:38:20 -0800

    scale parameters

--- src/tomography.jl
+++ src/tomography.jl
@@ -39,33 +39,44 @@ end
 
 function optimize_parameters(M::AxisymmetricEquilibrium, orbits::Vector, W::Matrix, d::Vector, err::Vector;
                              mu=Float64[], norm=1e18/size(W,2), ntot=(),
+                             dr = (0.01,0.5), dE = (1.0,20.0), dp=(0.01,1.0),
                              batch_size=1000, atol=1e-3, maxiter = 200, kwargs...)
 
     pool = CachingPool(workers())
     Js = pmap(x->get_jacobian(M, x; kwargs...), pool, orbits, on_error=x->zeros(length(x)), batch_size=batch_size)
 
+    n = length(orbits)
     ns = length.(orbits)
     ts = [range(0.0, 1.0, length=nn) for nn in ns]
     Jis = [make_jacobian_spline(jj,tt) for (jj, tt) in zip(Js,ts)]
     orbs = [OrbitSpline(o) for o in orbits]
 
-    f = function lml(orbs, Jis, atol, pool, batch_size, W, d, err, norm, mu, ntot, sigma)
+    f = function lml(Σ, orbs, Jis, atol, pool, batch_size, W, d, err, norm, mu, ntot, sigma)
         Σ_inv = S44(inv(Diagonal(sigma.^2)))
-        Σ = compute_covariance_matrix(orbs, Jis, Σ_inv, atol, pool; batch_size=batch_size)
-
+        Σ .= compute_covariance_matrix(orbs, Jis, Σ_inv, atol, pool; batch_size=batch_size)
+        Σ .= Σ + (1 + 1e-10)*abs(eigvals(Σ)[1])I #to make posdef
         op = optimize(x -> -marginal_loglike(W, d, err, (10^x)*Σ; norm=norm,mu=mu,ntot=ntot),
                       -6,3, Brent())
         return Optim.minimum(op)
     end
 
-    op = optimize(x -> f(orbs, Jis, atol, pool, batch_size, W, d, err, norm, mu, ntot, [x[1],x[2],x[3],x[3]]),
-                  [10.0, 0.3, 0.1, 0.1], NelderMead(),
-                  Optim.Options(show_trace=true, iterations=maxiter,show_every=10))
+    Σ = zeros(n,n)
+    op = optimize(x -> f(Σ, orbs, Jis, atol, pool, batch_size, W, d, err, norm, mu, ntot,
+                         [x[1]*(dE[2]-dE[1])+dE[1],
+                          x[2]*(dp[2]-dp[1])+dp[1],
+                          x[3]*(dr[2]-dr[1])+dr[1],
+                          x[3]*(dr[2]-dr[1])+dr[1]]),
+                  [0.5, 0.3, 0.2], NelderMead(),
+                  Optim.Options(show_trace=true, iterations=maxiter,show_every=1))
 
-    sig = Optim.minimizer(op)
-    sigma = [sig[1],sig[2],sig[3],sig[3]]
+    x = Optim.minimizer(op)
+    sigma = [x[1]*(dE[2]-dE[1])+dE[1],
+             x[2]*(dp[2]-dp[1])+dp[1],
+             x[3]*(dr[2]-dr[1])+dr[1],
+             x[3]*(dr[2]-dr[1])+dr[1]]
+
     Σ_x_inv = S44(inv(Diagonal(sigma.^2)))
-    Σ = compute_covariance_matrix(orbs, Jis, Σ_x_inv, atol, pool; batch_size=batch_size)
+    Σ .= compute_covariance_matrix(orbs, Jis, Σ_x_inv, atol, pool; batch_size=batch_size)
 
     op = optimize(x -> -marginal_loglike(W, d, err, (10^x)*Σ; norm=norm, mu=mu, ntot=ntot),
                  -6,3, Brent())
