61fc8829e2ee7f41bc7a81f174e0127a7c277efc
stuartbenj <u6377168@anu.edu.au>
2022-06-25 15:31:48 +1000

    polyharmonic splines more efficient matrix usage

--- src/polyharmonic.jl
+++ src/polyharmonic.jl
@@ -6,6 +6,14 @@ struct PolyharmonicSpline
     error::Float64
 end
 
+struct PolyharmonicSplineInv
+    dim::Int64
+    order::Int64
+    coeff::Vector{Float64}
+    centers::Array{Float64,2}
+    error::Float64
+end
+
 function polyharmonicK(r,K)
     if iseven(K)
         iszero(r) && return zero(r)
@@ -23,7 +31,12 @@ end
 
 function PolyharmonicSpline(K::Int64, centers::Array{Float64,2}, values::Array{Float64}; s = 0.0)
     m,n = size(centers)
-    m != length(values) && throw(DimensionMismatch())
+    if n == length(values)
+        @warn "Use PolyharmonicSplineInv instead of PolyharmonicSpline."
+        return PolyharmonicSplineInv(K, centers, values; s = s)
+    elseif m != length(values)
+        throw(DimensionMismatch())
+    end
 
     M = zeros(m,m)
     N = zeros(m,n+1)
@@ -57,8 +70,54 @@ function PolyharmonicSpline(K::Int64, centers::Array{Float64,2}, values::Array{F
     return PolyharmonicSpline(n,K,w,centers,error)
 end
 
+function PolyharmonicSplineInv(K::Int64, centers::Array{Float64,2}, values::Array{Float64}; s = 0.0)
+    n,m = size(centers)
+
+    if n == length(values)
+        @warn "Use PolyharmonicSpline instead of PolyharmonicSplineInv."
+        return PolyharmonicSpline(K, centers, values; s = s)
+    elseif m != length(values)
+        throw(DimensionMismatch())
+    end
+
+    M = zeros(m,m)
+    N = zeros(m,n+1)
+
+    for i=1:m
+        N[i,1] = 1
+        N[i,2:end] = centers[:,i]
+        for j=1:m
+            M[i,j] = polyharmonicK(norm(centers[:,i] .- centers[:,j]),K)
+        end
+    end
+    M = M + s*I
+    L = vcat(hcat(M,N),hcat(N', zeros(n+1,n+1)))
+
+    w = pinv(L)*vcat(values,zeros(n+1))
+
+    ivalues = zeros(m)
+    for i=1:m
+        tmp = 0.0
+        for j=1:m
+            tmp = tmp + w[j]*polyharmonicK(norm(centers[:,i] .- centers[:,j]),K)
+        end
+        tmp = tmp + w[m+1]
+        for j=2:n+1
+            tmp = tmp + w[m+j]*centers[j-1,i]
+        end
+        ivalues[i] = tmp
+    end
+    error = norm(values .- ivalues)
+
+    return PolyharmonicSplineInv(n,K,w,centers,error)
+end
+
 function PolyharmonicSpline(K::Int64, centers::Vector{Float64},values::Vector{Float64};s = 0.0)
     PolyharmonicSpline(K,reshape(centers,length(centers),1),values,s=s)
+end
+
+function PolyharmonicSplineInv(K::Int64, centers::Vector{Float64},values::Vector{Float64};s = 0.0)
+    PolyharmonicSpline(K,reshape(centers,1,length(centers)),values,s=s)
 end
 
 function (S::PolyharmonicSpline)(x::T...) where T <: Real
@@ -69,6 +128,24 @@ function (S::PolyharmonicSpline)(x::T...) where T <: Real
     l = length(S.coeff)-(n+1)
     for j=1:l
         v = v + S.coeff[j]*polyharmonicK(norm(x .- S.centers[j,:]), S.order)
+    end
+
+    v = v + S.coeff[l+1]
+    for j=2:n+1
+        v = v + S.coeff[l+j]*x[j-1]
+    end
+
+    return v
+end
+
+function (S::PolyharmonicSplineInv)(x::T...) where T <: Real
+    n = length(x)
+    n != S.dim && throw(DimensionMismatch("$n != $(S.dim)"))
+
+    v = 0.0
+    l = length(S.coeff)-(n+1)
+    for j=1:l
+        v = v + S.coeff[j]*polyharmonicK(norm(x .- S.centers[:,j]), S.order)
     end
 
     v = v + S.coeff[l+1]
